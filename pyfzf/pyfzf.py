0d775595c8a307cac1d2653138fb417b3c5527ef
sweatylobster <63132441+sweatylobster@users.noreply.github.com>
2023-04-03 07:07:44 +0000

    Update pyfzf.py
    Retrieves namespace objects instead of their string representation

--- pyfzf/pyfzf.py
+++ pyfzf/pyfzf.py
@@ -41,9 +41,12 @@ class FzfPrompt:
         else:
             self.executable_path = "fzf"
 
+
     def prompt(self, choices=None, fzf_options="", delimiter='\n'):
         # convert a list to a string [ 1, 2, 3 ] => "1\n2\n3"
         choices_str = delimiter.join(map(str, choices))
+        # store a map of fzf interface to python objects
+        retrieval = dict(zip(list(map(str, choices)), choices))
         selection = []
 
         with tempfile.NamedTemporaryFile(delete=False) as input_file:
@@ -52,11 +55,12 @@ class FzfPrompt:
                 input_file.write(choices_str.encode('utf-8'))
                 input_file.flush()
 
+        
         # Invoke fzf externally and write to output file
         os.system(
             f"{self.executable_path} {fzf_options} < \"{input_file.name}\" > \"{output_file.name}\"")
 
-        # get selected options
+        # get selected lines
         with open(output_file.name, encoding="utf-8") as f:
             for line in f:
                 selection.append(line.strip('\n'))
@@ -64,4 +68,5 @@ class FzfPrompt:
         os.unlink(input_file.name)
         os.unlink(output_file.name)
 
-        return selection
+        # now retrieve the selected *objects* passed in to the prompt
+        return [retrieval[key] for key in selection]
